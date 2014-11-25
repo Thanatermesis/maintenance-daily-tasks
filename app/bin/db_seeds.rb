1c556633ca8fa8f02cd2ec009d2e1d71d92b3008
sbartek <bartekskorulski@gmail.com>
2014-11-25 12:11:57 +0100

    adds date of instalation and last action

--- app/bin/db_seeds.rb
+++ app/bin/db_seeds.rb
@@ -1,3 +1,5 @@
+require 'date'
+
 require_relative 'app_helper'
 
 require 'csv'
@@ -10,13 +12,17 @@ mantenimientoCSV.each do |m|
   ficha = Ficha.get_or_create(m[1])
   equipo = Equipo.new
   equipo.ficha = ficha
+  if m[7] and m[7].match(/([0-9]{4,4})-([0-9]{2,2})-([0-9]{1,2})/)
+    d = m[7].match(/([0-9]{4,4})-([0-9]{2,2})-([0-9]{1,2})/)
+    equipo.instalacion=Date.new($1.to_i,$2.to_i,$3.to_i)
+  else
+    equipo.instalacion = Date.new(2014,12,15)+rand(60)
+  end
   equipo.save
   mantenimiento = Mantenimiento.new
   mantenimiento.ficha = ficha
   mantenimiento.descripcion = m[5]
-  puts Hash[Mantenimiento::PERIODICIDAD][m[4]]
   mantenimiento.periodicidad = Hash[Mantenimiento::PERIODICIDAD][m[4]]
-  puts mantenimiento
   mantenimiento.save
 end
 
--- db/db_schema.rb
+++ db/db_schema.rb
@@ -6,17 +6,19 @@ ActiveRecord::Schema.define do
 
   create_table :equipos do |table|
     table.column :ficha_id, :integer
+    table.column :instalacion, :date
   end
 
   create_table :mantenimientos do |table|
     table.column :ficha_id, :integer
-    table.column :descripcion, :integer
+    table.column :descripcion, :text
     table.column :periodicidad, :string
   end
 
   create_table :accions do |table|
     table.column :mantenimiento_id, :integer
     table.column :equipo_id, :integer
+    table.column :fecha, :datetime
   end
 
 end
