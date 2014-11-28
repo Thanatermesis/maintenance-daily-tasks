059a28d3f68ffef546a09a03d202e2435354a407
Thanatermesis <thanatermesis@gmail.com>
2014-11-28 15:53:45 +0100

    recovering old code from view_tasks

--- app/bin/view_tasks.rb
+++ app/bin/view_tasks.rb
@@ -1,18 +1,34 @@
 require_relative 'app_helper'
 
+# tasks = Task.all
+# fichas = Ficha.all
+# equipos = Equipos.all
 
-tasks = Equipo.pending_accions(DateTime.now)
+# win = Window.new({xsize:500})
+# glist  = GenList.new(win)
 
+# tasks.each do |t|
+#   GenListItem.new(glist, {text: t.name, icon: "home"})
+# end
+
+# win.e_exec
+
+
+
+tasks = Equipo.pending_accions(DateTime.now + 2.years)
+#p tasks
 
 win = Window.new({xsize:500})
-list  = List.new(win)
+glist  = GenList.new(win)
 
 tasks.keys.each do |e|
   tasks[e].keys.each do |m|
-    button = Button.new({text: "A tomar por ...", action: nil})
-    entry = ListItem.new(glist, { text: "#{e.ficha.nombre}: #{m.descripcion}", buttonleft: button})
+    entry = GenListItem.new(glist, { text: "#{e.ficha.nombre}: #{m.descripcion}", icon: "home", button: "wombat"})
+
+    #entry.add_action("clicked") do |a|
+      #puts "#{m.descripcion}"
+    #end
   end
 end
 
-#Accion.new
 win.e_exec
