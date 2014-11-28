1dfd1bd8fb4c57a0e28f4c7aa133f58910fd3bae
sbartek <bartekskorulski@gmail.com>
2014-11-28 16:28:01 +0100

    modifies table

--- app/bin/db_seeds2.rb
+++ app/bin/db_seeds2.rb
@@ -2,11 +2,16 @@ require_relative 'app_helper'
 
 require 'csv'
 
-mantenimientoCSV = CSV.read('app/assets/csv/acciones.csv')
+mantenimientoCSV = CSV.read('app/assets/csv/acciones.csv',  headers: true, :col_sep => '|')
 
-#Equipo.delete_all
-#Mantenimiento.delete_all
+fl = true
+Ficha.delete_all
+Equipo.delete_all
+Mantenimiento.delete_all
 mantenimientoCSV.each do |m|
-  puts m
-  #puts("#{m[1]}, #{m[5]}, #{m[4]}")
+  ficha = Ficha.get_or_create(m[1])
+  equipo = Equipo.new
+  equipo.ficha = ficha
+  equipo.instalacion = Date.new(2014,11,15)+rand(30)
+
 end
